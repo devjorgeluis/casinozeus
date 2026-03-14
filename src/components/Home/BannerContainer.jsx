ba35751d5edbdb58d1e4e2d99dbc227020349e0e
devjorgeluis <jorgencnc0608@gmail.com>
2026-03-14 18:23:19 +0800

    banner container

--- src/components/Home/BannerContainer.jsx
+++ src/components/Home/BannerContainer.jsx
@@ -1,63 +1,66 @@
 import { useNavigate } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 
 import ImgCasino from "/src/assets/img/casino-item.jpg";
 import ImgLiveCasino from "/src/assets/img/live-casino-item.jpg";
 import ImgSport from "/src/assets/img/sports-item.jpg";
+import ImgMobileCasino from "/src/assets/img/mobile-casino-item.jpg";
+import ImgMobileLiveCasino from "/src/assets/img/mobile-live-casino-item.jpg";
+import ImgMobileSport from "/src/assets/img/mobile-sports-item.jpg";
 
 const BannerContainer = ({ isSlotsOnly }) => {
     const navigate = useNavigate();
+    const { isMobile } = useOutletContext();
     const isSlotsOnlyMode = isSlotsOnly === "true" || isSlotsOnly === true;
 
     const menuItems = !isSlotsOnlyMode ? [
         {
             id: 'casino',
             name: 'Casino',
-            image: ImgCasino,
+            image: isMobile ? ImgMobileCasino : ImgCasino,
             href: '/casino'
         },
         {
             id: 'live-casino',
             name: 'Casino En Vivo',
-            image: ImgLiveCasino,
+            image: isMobile ? ImgMobileLiveCasino : ImgLiveCasino,
             href: '/live-casino',
         },
         {
             id: 'sports',
             name: 'Deportes',
-            image: ImgSport,
+            image: isMobile ? ImgMobileSport : ImgSport,
             href: '/sports'
         }
     ] : [
         {
             id: 'casino',
             name: 'Casino',
-            image: ImgCasino,
+            image: isMobile ? ImgMobileCasino : ImgCasino,
             href: '/casino'
         }
     ];
 
     return (
         <>
-            <div className="home-section">
-                <div className="home-title">
-                    <h3>
-                        <div className="title">
-                            Apuestas Deportivas
-                        </div>
-                    </h3>
-                </div>
-            </div>
-            <div className="home-section">
-                <div className="other-container">
-                    <div className="other">
+            <div className="home-section-module-important home-section-module-3 loaded">
+                <div className="home-section-module-container">
+                    <div className="dw-home-middle">
                         {menuItems.map((menu, index) => (
-                            <div className="item" key={index}>
-                                <img src={menu.image} alt={menu.name} />
-                                <div className="button-item">
-                                    <a onClick={() => navigate(menu.href)}>
-                                        <span>{menu.name}</span>
-                                    </a>
-                                </div>
+                            <div className="dw-item-figure" key={index}>
+                                <figure>
+                                    <img src={menu.image} alt={menu.name} />
+                                    <div className="over-hover">
+                                        <section>
+                                            <div className="over-hover-bottom">
+                                                <button className="btn btn-theme" onClick={() => navigate(menu.href)}>
+                                                    <span>{menu.name}</span>
+                                                </button>
+                                            </div>
+                                        </section>
+                                    </div>
+                                    <a onClick={() => navigate(menu.href)}></a>
+                                </figure>
                             </div>
                         ))}
                     </div>
